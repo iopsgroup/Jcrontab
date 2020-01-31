5e8d89daeee65b2abb2f4d8894e10eb09d7b1ac3
jeson <jeson@imoocc.com>
2020-01-31 17:09:10 +0800

    2020-01-31

--- Jcrontab/script/code_rsync.sh
+++ Jcrontab/script/code_rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #Email:jeson@imoocc.com
 
-rsync -avzP -e 'ssh -i /opt/key/jesonc.com' /Users/jeson/PycharmProjects/Jcron root@jesonc.com:/opt/codes
+rsync -avzP -e 'ssh -i /opt/key/jesonc.com -p 9922' /Users/jeson/PycharmProjects/Jcron root@jesonc.com:/opt/codes
